0a3311b81948010e227c45b1763cd17e3621c860
Ndiaye <0sansnom200@gmail.com>
2021-07-07 12:32:03 +0200

    testing firebase

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const navbar = document.querySelector("#nav")
 const navBtn = document.querySelector("#nav-btn")
-const closeBtn = document.querySelector("#close-btn");
+const closeBtn = document.querySelector("#close-btn")
 const sidebar = document.querySelector("#sidebar")
+const sidebarTo = document.querySelectorAll(".sidebar-link")
 const date = document.querySelector("#date")
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
@@ -13,14 +14,16 @@ window.addEventListener("scroll", function () {
 })
 // show sidebar
 navBtn.addEventListener("click", function () {
-  sidebar.classList.add("show-sidebar");
-});
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
+  sidebar.classList.add("show-sidebar")
 })
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar")
+})
+
+sidebarTo.forEach(element => {
+  element.addEventListener("click", function(){
+    sidebar.classList.remove("show-sidebar")
+  } )
+});
 // set year
 date.innerHTML = new Date().getFullYear()
-
-
-
-
